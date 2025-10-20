b8fc4e0d049d6a464e041fc1ef0a87ea28d2a692
Jason <jasn74939@gmail.com>
2025-10-20 13:24:11 -0400

    add event listeners to lang.js

--- src/frontend/js/lang.js
+++ src/frontend/js/lang.js
@@ -3,4 +3,21 @@ const css = document.getElementById('lang-css');
 const html = document.getElementById('lang-html');
 const javascript = document.getElementById('lang-javascript');
 const python = document.getElementById('lang-python');
-const typescript = document.getElementById('lang-typescript');
+
+java.addEventListener('click', async () => { 
+}
+
+css.addEventListener('click', async () => {
+}  
+
+html.addEventListener('click', async () => {
+}
+
+javascript.addEventListener('click', async () => {
+}
+
+python.addEventListener('click', async () => {
+}
+
+                        
+                      

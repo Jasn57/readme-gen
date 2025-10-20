32396d6efa250ebb35243ff9d615bbab7aee87ba
Jason <jasn74939@gmail.com>
2025-10-20 13:27:07 -0400

    Add event listeners to tools.js
    Added event listeners for various tools including VirtualBox.

--- src/frontend/js/tools.js
+++ src/frontend/js/tools.js
@@ -3,3 +3,18 @@ const figma = document.getElementById('tool-figma');
 const github = document.getElementById('tool-github');
 const linux = document.getElementById('tool-linux');
 const virtualbox = document.getElementById('tool-virtualbox');
+
+vscode.addEventListener('click', async () => {
+}
+
+figma.addEventListener('click', async () => {
+}
+
+github.addEventListener('click', async () => {
+}
+
+linux.addEventListener('click', async () => {
+}
+
+virtualbox.addEventListener('click', async () => {
+}                        

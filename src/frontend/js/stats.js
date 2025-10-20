51ad5e71438e616de683895a9ed6a39f21bed8de
Jason <jasn74939@gmail.com>
2025-10-20 13:25:38 -0400

    Add event listeners to stats.js

--- src/frontend/js/stats.js
+++ src/frontend/js/stats.js
@@ -1,3 +1,9 @@
 const languages = document.getElementById('stat-langgraph');
 const commits = document.getElementById('stat-commitgraph');
 
+languages.addEventListener('click', async () => {
+}
+
+commits.addEventListener('click', async () => {
+}
+                           
